1fbe0ae6b1ccd6908a80fd073765576fcdfd9f49
Alejandro P <aleponce92@gmail.com>
2024-06-22 14:01:27 -0500

    More robust code and error handling

--- menu_functions.py
+++ menu_functions.py
@@ -1,77 +1,47 @@
-# menu_functions.py - Contains all the functions related to the menu items, such as import_data, label_peaks, adjust_spectrum, adjust_plot, etc.
-
-import tkinter as tk
-from tkinter import ttk  # ttk is a submodule of tkinter for themed widgets
-from tkinter import Toplevel
-from tkinter import filedialog
-from tkinter import messagebox
-import tkinter.font as font
-import pandas as pd
 import os
+import tkinter as tk
+from tkinter import ttk, filedialog, messagebox
+import pandas as pd
 import functools
+from PIL import Image, ImageTk
 from search_element import search_element, periodic_table_window
 from graph_space import update_title
 import graph_space
 import live_graph_space
-from tkinter import PhotoImage
-from PIL import ImageTk, Image
 import numpy as np
-import importlib
+from adjust_spectrum import adjust_spectrum
+from adjust_plot import adjust_plot
 
-
+# Function to create acquisition buttons in the sidebar
 def create_acquisition_buttons(app):
-    # This function creates the buttons for the "Acquisition" mode
-
-    # Create a new frame to hold the buttons
     buttons_frame = ttk.LabelFrame(app.root.sidebar_frame)
     buttons_frame.grid(row=1, column=0, padx=20, pady=2)
 
-    # Create placeholder buttons
-    # (replace these with actual buttons later)
-    icon_size = (40, 40)  # Define the desired icon size
-    placeholder_icon_path = "Icons/Import_icon.png"  # Placeholder icon path (replace with actual icon path later)
+    icon_size = (40, 40)
+    placeholder_icon_path = "Icons/Import_icon.png"
     for i in range(5):
-        # Resize the icon
         icon_image = Image.open(placeholder_icon_path).resize(icon_size, Image.ANTIALIAS)
         icon_photo = ImageTk.PhotoImage(icon_image)
-
-        # Create the button with the icon and left-justified text
         btn = ttk.Button(buttons_frame, text=f"Placeholder Button {i+1}", image=icon_photo, compound='left', style="LeftAligned.TButton", width=20)
         btn.grid(row=i, column=0, padx=10, pady=15)
-        btn.icon_photo = icon_photo  # Keep a reference to the icon to prevent garbage collection
-
-    # return the frame containing the buttons
+        btn.icon_photo = icon_photo
     return buttons_frame
 
-#=======================================================================================================================
-# Creates the tkk sidebar and adds it to the main window:
+# Function to create the sidebar
 def create_sidebar(app):
-
-    # Create the sidebar frame:
     app.root.sidebar_frame = ttk.Frame(app.root, width=190)
     app.root.sidebar_frame.place(x=0, y=0, relwidth=0.1, relheight=1)
 
-    # Create a new frame to hold the buttons and labels
     buttons_frame = ttk.LabelFrame(app.root.sidebar_frame)
     buttons_frame.grid(row=1, column=0, padx=30, pady=2)
 
-    # Create a new style for the emphasized label
     style = ttk.Style()
     style.configure("Emphasized.TLabel", font=("lato", 16, "bold"), foreground="black")
-
-    # Create the sidebar label:
     app.root.sidebar_label = ttk.Label(app.root.sidebar_frame, text="Menu", style="Emphasized.TLabel")
     app.root.sidebar_label.grid(row=0, column=0, padx=10, pady=(60, 2))
 
-    # search_element_handler, that takes the required argument selected_element from the app instance and calls search_element with it
-    def search_element_handler(app):
-        selected_element = app.selected_element.get()
-        search_element(app, selected_element)
-
-    # Create a new style for the left-aligned button
     style.configure("LeftAligned.TButton", anchor='w')
 
-    # Create the icons and their respective functions (import data, plot element, etc.):
     icons = [
         ("Import Data", "Icons/Import_icon.png", functools.partial(import_data, app)),
         ("Adjust Spectrum", "Icons/spectrum_icon.png", functools.partial(adjust_spectrum, app, app.ax)),
@@ -80,115 +50,68 @@ def create_sidebar(app):
         ("Export Plot", "Icons/export_icon.png", functools.partial(export_plot, app, app.ax)),
         ("Export Data", "Icons/savedata_icon.png", functools.partial(export_data, app)),
         ("Clean Plot", "Icons/clean_icon.png", functools.partial(clean_plot, app)),
-        
     ]
 
-    icon_size = (40, 40)  # Define the desired icon size
-  
-    # separate function to handle the button click
-    def button_click_handler(app, command):
-        command(app)
-
-    # Create the buttons for each icon:
+    icon_size = (40, 40)
     for i, (text, icon_path, command) in enumerate(icons):
-        # Resize the icon
         icon_image = Image.open(icon_path).resize(icon_size, Image.ANTIALIAS)
         icon_photo = ImageTk.PhotoImage(icon_image)
-
-        # Create the button with the icon and left-justified text
         btn = ttk.Button(buttons_frame, text=text, image=icon_photo, compound='left', command=command, style="LeftAligned.TButton", width=20)
         btn.grid(row=i, column=0, padx=10, pady=15)
-        btn.icon_photo = icon_photo  # Keep a reference to the icon to prevent garbage collection
+        btn.icon_photo = icon_photo
 
-    # Load the logo:    
     logo = Image.open("Icons/Onteko_Logo.JPG")
-
-    # Calculate the new dimensions while maintaining the aspect ratio
-    original_width = 1063
-    original_height = 344
+    original_width, original_height = 1063, 344
     max_width = 200
     new_height = int((max_width / original_width) * original_height)
-
-    # Resize the logo
     logo_resized = logo.resize((max_width, new_height), Image.ANTIALIAS)
-    # Convert the resized logo to a PhotoImage
     logo_image = ImageTk.PhotoImage(logo_resized)
-
-    # Create a separate frame for the logo
     logo_frame = ttk.Frame(app.root.sidebar_frame)
     logo_frame.place(x=0, rely=1.0, y=-new_height, anchor='sw', height=new_height, width=200)
-    logo_frame.grid(row=60, column=0, padx=(10,1), pady=(140, 5))
+    logo_frame.grid(row=60, column=0, padx=(10, 1), pady=(140, 5))
 
-
-#=======================================================================================================================
-
-# Define reset_data function:
+# Function to reset data
 def reset_data(app):
     app.data = pd.DataFrame()
     app.x_data = pd.Series()
     app.y_data = pd.Series()
 
-
-# define import button function
+# Function to import data from files
 def import_data(app):
-    reset_data(app)  # Reset the data before importing a new file
+    reset_data(app)
     filetypes = [("Text files", "*.txt"), ("All files", "*.*")]
-    file_path = filedialog.askopenfilenames(title="Select data files", filetypes=filetypes)  # Allows multiple file selection
+    file_paths = filedialog.askopenfilenames(title="Select data files", filetypes=filetypes)
 
-    # Initialize an empty DataFrame to store data from all files
     all_data = pd.DataFrame()
-
-    for path in file_path:
+    for path in file_paths:
         with open(path, 'r') as file:
             file_content = file.read()
 
-        # Determine the decimal separator and delimiter
         decimal_separator = ',' if ',' in file_content and '.' not in file_content else '.'
         delimiter = '\t' if '\t' in file_content else ','
-
-        # Read the CSV file with the correct delimiter and decimal separator
         data = pd.read_csv(path, sep=delimiter, engine='python', header=None, decimal=decimal_separator, skiprows=1)
-        
-        # Append the data from each file
         all_data = pd.concat([all_data, data], axis=0)
 
-    # Group by the first column (wavelength), and calculate the mean of the second column (intensity)
     if not all_data.empty:
         averaged_data = all_data.groupby(0).mean().reset_index()
-        x = averaged_data[0]
-        y = averaged_data[1]
+        app.x_data, app.y_data = averaged_data[0], averaged_data[1]
+        app.data = averaged_data
+        app.ax.clear()
+        app.ax.plot(app.x_data, app.y_data)
+        app.line = app.ax.lines[-1]
+        app.ax.set_xlim([100, 1000])
+        app.ax.set_xlabel("Wavelength (nm)")
+        app.ax.set_ylabel("Relative Intensity")
+        app.ax.grid(which='both', linestyle='--', linewidth=0.5)
+        file_name = ", ".join(os.path.basename(path) for path in file_paths)
+        update_title(app, file_name)
+        app.canvas.draw()
     else:
-        x, y = pd.Series(), pd.Series()  # Handle the case where no files are selected
+        app.x_data, app.y_data = pd.Series(), pd.Series()
+        app.ax.clear()
+        app.canvas.draw()
 
-    # Update the x_data and y_data attributes in the App class
-    app.x_data = x
-    app.y_data = y
-    app.data = averaged_data
-
-    # Clear the current plot
-    app.ax.clear()
-
-    # Plot the new data
-    app.ax.plot(x, y)
-    app.line = app.ax.lines[-1]  # Store the line object in the app class
-
-    # Reapply the grid and axis labels
-    app.ax.set_xlim([100, 1000])
-    app.ax.set_xlabel("Wavelength (nm)")
-    app.ax.set_ylabel("Relative Intensity")
-    app.ax.grid(which='both', linestyle='--', linewidth=0.5)
-
-    # Extract the file name from the file path
-    file_name = ", ".join(os.path.basename(path) for path in file_path)
-    # Set the graph title based on the CSV file name    
-    update_title(app, file_name)
-
-    # Redraw the canvas
-    app.canvas.draw()
-
-
-#=======================================================================================================================
-# Define the clean_plot function:
+# Function to clean the plot
 def clean_plot(app):
     app.ax.cla()
     app.ax.set_xlim([100, 1000])
@@ -197,21 +120,17 @@ def clean_plot(app):
     app.ax.grid(which='both', linestyle='--', linewidth=0.5)
     app.canvas.draw()
 
-# Define the adjust_spectrum function:
+# Function to adjust the spectrum
 def adjust_spectrum(app, ax):
-    from tkinter import Toplevel
-    from adjust_spectrum import adjust_spectrum
     adjust_spectrum(app, ax)
 
-# Define the adjust_plot function:
+# Function to adjust the plot
 def adjust_plot(app, ax):
-    from adjust_plot import adjust_plot
     adjust_plot(app, ax)
 
-# Define the export_plot function:
+# Function to export the plot
 def export_plot(app, ax):
     def ask_save_file_and_dpi():
-        # Create a Toplevel window for DPI input
         dpi_window = tk.Toplevel(app.root)
         dpi_window.title("DPI Option")
 
@@ -227,37 +146,23 @@ def export_plot(app, ax):
             file_path = filedialog.asksaveasfilename(defaultextension='.pdf', filetypes=filetypes)
 
             if file_path:
-                # Extract the file extension
                 file_ext = os.path.splitext(file_path)[1]
-
-                # Set the appropriate filetype
                 filetype = file_ext[1:]
-
-                # Save the plot with the selected filetype and dpi
                 ax.figure.savefig(file_path, format=filetype, dpi=dpi)
-
-                # Show a message box indicating that the export was successful
                 messagebox.showinfo("Export Successful", f"The plot was successfully exported as {file_ext.upper()}")
-
-
-            # Close the DPI input window
             dpi_window.destroy()
 
-        # Create and place the save button
         save_button = ttk.Button(dpi_window, text="Save", command=save_file)
         save_button.grid(row=1, column=0, columnspan=2, pady=10)
 
     ask_save_file_and_dpi()
 
-
-
-#=======================================================================================================================
+# Function to export the data
 def export_data(app):
     filetypes = [("CSV files", "*.csv"), ("Excel files", "*.xlsx")]
     file_path = filedialog.asksaveasfilename(title="Select a file", filetypes=filetypes, defaultextension=".csv")
-    
+
     if file_path:
-        # Convert the peak data to a pandas DataFrame
         if not app.peak_data:
             messagebox.showinfo("No data", "No peak data to export.")
             return
@@ -266,7 +171,7 @@ def export_data(app):
         file_extension = os.path.splitext(file_path)[1]
         if file_extension == ".csv":
             df.to_csv(file_path, index=False)
-        elif file_extension == ".xlsx": 
+        elif file_extension == ".xlsx":
             df.to_excel(file_path, index=False)
         messagebox.showinfo("Export Successful", f"The data was successfully exported to {file_path}")
 
